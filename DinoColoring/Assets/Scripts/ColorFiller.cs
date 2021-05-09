d5aef51d33c2767367dcd83aaf9f09522edd9e4b
Eric Oliver <eroliver27@hotmail.com>
2021-05-08 23:51:26 -0400

    adding in basic scripts

---
+++ DinoColoring/Assets/Scripts/ColorFiller.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFiller : MonoBehaviour
+{
+    private SpriteRenderer maskSpriterendere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maskSpriterendere = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeColor()
+    {
+        
+    }
+}
---
+++ DinoColoring/Assets/Scripts/PaintBrush.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintBrush : MonoBehaviour
+{
+    private Dictionary<int, string> colorOptions = new Dictionary<int, string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
