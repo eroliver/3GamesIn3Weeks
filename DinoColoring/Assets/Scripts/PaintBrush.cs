392bbde6aee33381ad10ec5b4853ee9633465aba
eroliver <eroliver27@hotmail.com>
2021-05-11 19:04:23 -0400

    pushed for remote work

--- DinoColoring/Assets/Scripts/PaintBrush.cs
+++ DinoColoring/Assets/Scripts/PaintBrush.cs
@@ -6,20 +6,24 @@ public class PaintBrush : MonoBehaviour
 {
     private Color[] colorOptions = new Color[4];
     private Color activeColor;
-    private Camera gameCamera;
+    [SerializeField]
+    private Camera gameCamera;  
 
 
     // Start is called before the first frame update
     void Start()
     {
-        activeColor = Color.white;
-        gameCamera = GetComponent<Camera>();
+        activeColor = Color.green;
+        //gameCamera = GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetPaintableArea();
+        }
     }
 
     public void ChangeColor()
@@ -30,5 +34,25 @@ public class PaintBrush : MonoBehaviour
             Color chosenColor = chosenPaint.color;
             activeColor = chosenColor;
         }
+    }
+
+    public void GetPaintableArea()
+    {
+        Ray ray = gameCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        Transform objectClicked = null;
+        Color clickedObjectColor = Color.white;
+        if (Physics.Raycast(ray, out hit))
+        {
+            objectClicked = hit.transform;
+            clickedObjectColor = objectClicked.gameObject.GetComponent<SpriteRenderer>().color;
+            Debug.Log("click");
+            if (objectClicked.tag == "paintable")
+            {
+                clickedObjectColor = activeColor;
+            }
+        }
+        
+
     }
 }
